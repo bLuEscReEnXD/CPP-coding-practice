4d8fe454dde9dcfb753cedc76e7dea577aa84a34
bLuEscReEn <83490669+bLuEscReEnXD@users.noreply.github.com>
2024-09-07 16:41:14 +0700

    Update 64-Minimum Path Sum.cpp

--- DP/64-Minimum Path Sum.cpp
+++ DP/64-Minimum Path Sum.cpp
@@ -1,7 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-
 class Solution {
 
     public:
@@ -27,53 +23,10 @@ class Solution {
                 }
             }
 
-            /*for(int i = 0;i <m; i++){
-                for(int j=0; j<n; j++){
-                    cout << DP[i][j] << '\t';
-                }
-                cout << endl;
-            }*/
             return DP[m-1][n-1];
 
         }
 };
-
-    public:
-        int minPathSum(vector<vector<int>>& grid) {
-
-            int m = grid.size(), n = grid[0].size();
-
-            //vector<vector<int>> DP(m, vector<int>(n,-1));
-
-            //DP[0][0] = grid[0][0];
-
-            for(int i = 1;i <m; i++){
-                grid[0][i] += grid[0][i-1];
-            }
-
-            for(int i = 1;i <n; i++){
-                grid[i][0] += grid[i-1][0];
-            }
-
-            for(int i = 1;i <m; i++){
-                for(int j=1; j<n; j++){
-                    grid[i][j] += min(grid[i][j-1], grid[i-1][j]);
-                }
-            }
-
-
-
-            /*for(int i = 0;i <m; i++){
-                for(int j=0; j<n; j++){
-                    cout << DP[i][j] << '\t';
-                }
-                cout << endl;
-            }*/
-            return grid[m-1][n-1];
-
-        }
-};
-
 
 int main(){
 
@@ -82,4 +35,3 @@ int main(){
     cout << Solution().minPathSum(grid);
 
     return 0;
-}
